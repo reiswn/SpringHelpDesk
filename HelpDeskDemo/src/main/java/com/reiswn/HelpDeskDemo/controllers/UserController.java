84ad21dd77448cb99d428328157eb65f61ef1281
Willian <reiswn@gmail.com>
2020-03-06 19:53:09 -0300

    User controller created

---
+++ HelpDeskDemo/src/main/java/com/reiswn/HelpDeskDemo/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.reiswn.HelpDeskDemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.reiswn.HelpDeskDemo.models.User;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping
+	public String index(Model model) {
+		return "users/index";
+	}
+	
+	@GetMapping("/new")
+	public String create(Model model) {
+		model.addAttribute("user", new User());
+		return "users/create";
+	}
+	
+	@GetMapping("/{id}")
+	public String edit(@PathVariable("id") Long id, Model model) {
+		return "users/edit";
+	}
+	
+	
+}
