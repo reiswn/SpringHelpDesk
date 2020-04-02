af6adc764af6dee9b551ac2080ef63faffbcb71c
Willian <reiswn@gmail.com>
2020-04-02 01:33:26 -0300

    pdf itext report add

--- HelpDeskDemo/src/main/java/com/reiswn/HelpDeskDemo/controllers/ReportsController.java
+++ HelpDeskDemo/src/main/java/com/reiswn/HelpDeskDemo/controllers/ReportsController.java
@@ -1,13 +1,21 @@
 package com.reiswn.HelpDeskDemo.controllers;
 
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.reiswn.HelpDeskDemo.models.Ticket;
 import com.reiswn.HelpDeskDemo.services.TicketService;
+import com.reiswn.HelpDeskDemo.util.TicketReportPdf;
 
 @Controller
 @RequestMapping("/reports")
@@ -26,5 +34,23 @@ public class ReportsController {
 	public String ticketReport(@RequestParam(required = false, value = "day") Integer day, Model model) {
 		model.addAttribute("list", this.ticketService.reportTicketByDays(day));
 		return "reports/ticket";
+	}
+	
+	@GetMapping("/tickets/pdfgen")
+	public String ticketReportPdfForm(Model model) {
+		return "reports/ticket_pdf";
+	}
+	
+	@GetMapping(value = "/tickets/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<InputStreamResource> pdfTicketReport(@RequestParam(required = false, value = "day") Integer day, Model model) {
+		List<Ticket> tickets = this.ticketService.reportTicketByDays(day);
+				
+		ByteArrayInputStream pdf = TicketReportPdf.generate(tickets);
+		
+		return ResponseEntity
+					.ok()
+					.header("Content-Disposition", "inline; filname=report.pdf")
+					.contentType(MediaType.APPLICATION_PDF)
+					.body(new InputStreamResource(pdf));
 	}
 }
